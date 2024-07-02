27b3cbd90ab39dda7868a7ef90b8555542ca4481
D4RKC6L0 <162716285+D4RKC6L0@users.noreply.github.com>
2024-07-02 09:33:37 -0300

    Add files via upload

---
+++ clone/aula-keliven-/aula-keliven-/js/index.js
@@ -0,0 +1,15 @@
+function alertYes(){
+    alert("NÃO ESUQEÇAM, DIA 14/07")
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
